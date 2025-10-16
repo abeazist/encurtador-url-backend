ac20ea591fe4b4a3a5d787d28e50aa9754109110
beatriz <128046453+abeazist@users.noreply.github.com>
2025-10-16 16:49:45 -0300

    Update database.js

--- src/infra/database.js
+++ src/infra/database.js
@@ -1,15 +1,18 @@
-
 import pg from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const { Pool } = pg;
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
 
 pool.on('connect', () => {
-  console.log(' Conectado ao banco PostgreSQL');
+  console.log('Conectado ao banco PostgreSQL');
 });
 
 export default pool;
