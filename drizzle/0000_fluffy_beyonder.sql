c7c549b2d277e09bd9df2a2d8831a1c473fe73b5
beatriz <128046453+abeazist@users.noreply.github.com>
2025-10-19 18:45:36 -0300

    exclui o bd antigo e criei um novo

---
+++ drizzle/0000_fluffy_beyonder.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "links" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"idLinkEncurtado" varchar(255) NOT NULL,
+	"legenda" varchar(255) NOT NULL,
+	"urlOriginal" varchar(255) NOT NULL,
+	"dataCriacao" timestamp DEFAULT now()
+);
--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,15 +1,12 @@
-import { pgTable, unique, serial, varchar, text, integer, timestamp } from "drizzle-orm/pg-core"
+import { pgTable, serial, varchar, timestamp } from "drizzle-orm/pg-core"
 import { sql } from "drizzle-orm"
 
 
 
 export const links = pgTable("links", {
 	id: serial().primaryKey().notNull(),
-	codigo: varchar({ length: 10 }).notNull(),
-	legenda: varchar({ length: 255 }),
-	urlOriginal: text("urlOriginal").notNull(),
-	clicks: integer().default(0),
-	createdAt: timestamp("created_at", { mode: 'string' }).defaultNow(),
-}, (table) => [
-	unique("links_codigo_key").on(table.codigo),
-]);
+	idLinkEncurtado: varchar({ length: 255 }).notNull(),
+	legenda: varchar({ length: 255 }).notNull(),
+	urlOriginal: varchar({ length: 255 }).notNull(),
+	dataCriacao: timestamp({ mode: 'string' }).defaultNow(),
+});
