20e0046932c7460139fbddf7fec3695b73866518
beatriz <128046453+abeazist@users.noreply.github.com>
2025-10-21 20:55:15 -0300

    dando erro de integer no drizzle

--- drizzle/0000_bitter_catseye.sql
+++ drizzle/0000_bitter_catseye.sql
@@ -1,8 +1,13 @@
+-- Current sql file was generated after introspecting the database
+-- If you want to run this migration please uncomment this code before executing migrations
+/*
 CREATE TABLE "links" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"idLinkEncurtado" varchar(255) NOT NULL,
 	"legenda" varchar(255) NOT NULL,
 	"urlOriginal" varchar(255) NOT NULL,
 	"dataCriacao" timestamp DEFAULT now(),
-	"num_cliques" INTEGER DEFAULT 0 NOT NULL
+	"num_cliques" int DEFAULT 0
 );
+
+*/
--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,12 +1,11 @@
-import { pgTable, serial, varchar, timestamp, integer } from "drizzle-orm/pg-core"
-import { sql } from "drizzle-orm"
-
+import { pgTable, serial, varchar, timestamp, integer } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
 
 export const links = pgTable("links", {
-	id: serial().primaryKey().notNull(),
-	idLinkEncurtado: varchar({ length: 255 }).notNull(),
-	legenda: varchar({ length: 255 }).notNull(),
-	urlOriginal: varchar({ length: 255 }).notNull(),
-	dataCriacao: timestamp({ mode: 'string' }).defaultNow(),
-	numCliques: integer("num_cliques").default(0).notNull(),
+  id: serial("id").primaryKey().notNull(),
+  idLinkEncurtado: varchar("id_link_encurtado", { length: 255 }).notNull(),
+  legenda: varchar("legenda", { length: 255 }).notNull(),
+  urlOriginal: varchar("url_original", { length: 255 }).notNull(),
+  dataCriacao: timestamp("data_criacao", { mode: "string" }).defaultNow(),
+  numCliques: integer("num_cliques").default(sql`0`).notNull(), // 👈 usa sql`0`
 });
